5678041568de80af155d00f875db528a904a2de7
augmentchinedu <augmentchinedu@gmail.com>
2026-01-18 07:35:09 +0000

    Package Changed To GraphQL Setup

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -1,10 +1,21 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+// graphql/schema/index.js
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const load = (file) =>
-  readFileSync(join(process.cwd(), "graphql/schema", file), "utf8");
+// Make __dirname work in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Helper to load a file relative to this module
+const loadFromModule = (file) =>
+  fs.readFileSync(path.join(__dirname, file), "utf8");
+
+// If you also want to support loading from the current working directory (optional):
+// const loadFromCWD = (file) => fs.readFileSync(path.join(process.cwd(), "graphql/schema", file), "utf8");
+
+// Merge your schema files
 export const typeDefs = `
-  ${load("file.gql")}
-  ${load("query.gql")}
+${loadFromModule("file.gql")}
+${loadFromModule("query.gql")}
 `;
